b53548a2bb89f26ae1c1c8c76383e5b174350564
jalal-akbar <jalal.albarn@gmail.com>
2024-03-06 22:00:06 +0800

    cors

--- cors/main.go
+++ cors/main.go
@@ -1,44 +1,69 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/rs/cors"
 )
 
 func main() {
-	http.HandleFunc("/cors", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://www.google.com")
-		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
-		if r.Method == "OPTIONS" {
-			w.Write([]byte("allowed"))
-			return
-		}
-		w.Write([]byte("hello"))
+	e := echo.New()
+	// cors middleware
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins: []string{"https://www.google.com, https://novalagung.com"},
+		AllowedMethods: []string{http.MethodOptions, http.MethodGet, http.MethodPost, http.MethodPut},
+		AllowedHeaders: []string{"Content-Type, X-CRSF-Token"},
+		Debug:          true,
 	})
-	// multiple origin
-	http.HandleFunc("/multiplecors", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://www.google.com, https://novalagung.com")
-		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
-		if r.Method == "OPTIONS" {
-			w.Write([]byte("allowed"))
-			return
-		}
-		w.Write([]byte("hello"))
-	})
-	// allow all
-	http.HandleFunc("/allowallcors", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.Write([]byte("allowed"))
-			return
-		}
-		w.Write([]byte("hello"))
+	e.Use(echo.WrapMiddleware(corsMiddleware.Handler))
+
+	e.GET("/corsmiddleware", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "Hello")
 	})
 
-	log.Println("Starting app at :9000")
-	http.ListenAndServe(":9000", nil)
+	e.Logger.Fatal(e.Start(":9000"))
+
+	// http.HandleFunc("/cors", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Access-Control-Allow-Origin", "https://www.google.com")
+	// 	w.Header().Set("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT")
+	// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
+	// 	if r.Method == "OPTIONS" {
+	// 		w.Write([]byte("allowed"))
+	// 		return
+	// 	}
+	// 	w.Write([]byte("hello"))
+	// })
+	// // multiple origin
+	// http.HandleFunc("/multiplecors", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Access-Control-Allow-Origin", "https://www.google.com, https://novalagung.com")
+	// 	w.Header().Set("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT")
+	// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
+	// 	if r.Method == "OPTIONS" {
+	// 		w.Write([]byte("allowed"))
+	// 		return
+	// 	}
+	// 	w.Write([]byte("hello"))
+	// })
+	// // allow all
+	// http.HandleFunc("/allowallcors", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// 	w.Header().Set("Access-Control-Allow-Methods", "*")
+	// 	w.Header().Set("Access-Control-Allow-Headers", "*")
+	// 	if r.Method == "OPTIONS" {
+	// 		w.Write([]byte("allowed"))
+	// 		return
+	// 	}
+	// 	w.Write([]byte("hello"))
+	// })
+	// // preflight request
+	// http.HandleFunc("/preflight", func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.Method == "OPTIONS" {
+	// 		w.Write([]byte("allowed"))
+	// 		return
+	// 	}
+	// })
+
+	// log.Println("Starting app at :9000")
+	// http.ListenAndServe(":9000", nil)
 }
